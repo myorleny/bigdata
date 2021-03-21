83c1dc410b910c61520f0e75ae17daf0f0f8eece
Yorleni Alfaro <yorlenialfaro@gmail.com>
2021-03-20 21:33:17 -0600

    Configuracion pruebas

--- tarea1/tarea1.py
+++ tarea1/tarea1.py
@@ -5,63 +5,67 @@ from pyspark.sql.functions import col, date_format, udf
 from pyspark.sql.types import (DateType, IntegerType, FloatType, StringType,
                                StructField, StructType, TimestampType)
 
-spark = SparkSession.builder.appName("Read Transactions").getOrCreate()
 
-csv_schema = StructType([StructField('customer_id', IntegerType()),
-                         StructField('amount', FloatType()),
-                         StructField('purchased_at', TimestampType()),
-                         ])
+def join_dataframes(left, right, columns_left, columns_right):
+    pass
 
-dataframe = spark.read.csv("transactions.csv",
-                           schema=csv_schema,
-                           header=True)
+# spark = SparkSession.builder.appName("Read Transactions").getOrCreate()
 
-dataframe.show()
+# csv_schema = StructType([StructField('customer_id', IntegerType()),
+#                          StructField('amount', FloatType()),
+#                          StructField('purchased_at', TimestampType()),
+#                          ])
 
-# Add a new column by formatting the original date
+# dataframe = spark.read.csv("transactions.csv",
+#                            schema=csv_schema,
+#                            header=True)
 
-formatted_df = dataframe.withColumn("date_string",
-                                    date_format(col("purchased_at"),
-                                                'MM/dd/yyyy'))
-formatted_df.show()
+# dataframe.show()
 
-# Create a user defined function
-string_to_date = \
-    udf(lambda text_date: datetime.strptime(text_date, '%m/%d/%Y'),
-        DateType())
+# # Add a new column by formatting the original date
 
-typed_df = formatted_df.withColumn(
-    "date", string_to_date(formatted_df.date_string))
-typed_df.show()
-typed_df.printSchema()
+# formatted_df = dataframe.withColumn("date_string",
+#                                     date_format(col("purchased_at"),
+#                                                 'MM/dd/yyyy'))
+# formatted_df.show()
 
-# Group By and Select the data already aggregated
-sum_df = typed_df.groupBy("customer_id", "date").sum()
-sum_df.show()
+# # Create a user defined function
+# string_to_date = \
+#     udf(lambda text_date: datetime.strptime(text_date, '%m/%d/%Y'),
+#         DateType())
 
-stats_df = \
-    sum_df.select(
-        col('customer_id'),
-        col('date'),
-        col('sum(amount)').alias('amount'))
+# typed_df = formatted_df.withColumn(
+#     "date", string_to_date(formatted_df.date_string))
+# typed_df.show()
+# typed_df.printSchema()
 
-stats_df.printSchema()
-stats_df.show()
+# # Group By and Select the data already aggregated
+# sum_df = typed_df.groupBy("customer_id", "date").sum()
+# sum_df.show()
 
-# Load separate file where we store user names...
-name_schema = StructType([StructField("id", IntegerType()),
-                          StructField("name", StringType()),
-                          StructField("currency", StringType())])
+# stats_df = \
+#     sum_df.select(
+#         col('customer_id'),
+#         col('date'),
+#         col('sum(amount)').alias('amount'))
 
-names_df = spark.read.csv('names.csv',
-                          schema=name_schema,
-                          header=True)
+# stats_df.printSchema()
+# stats_df.show()
 
-names_df.printSchema()
-names_df.show()
+# # Load separate file where we store user names...
+# name_schema = StructType([StructField("id", IntegerType()),
+#                           StructField("name", StringType()),
+#                           StructField("currency", StringType())])
 
-# ...and join to the aggregates
-joint_df = stats_df.join(names_df, stats_df.customer_id == names_df.id)
-joint_df.printSchema()
-joint_df.show()
+# names_df = spark.read.csv('names.csv',
+#                           schema=name_schema,
+#                           header=True)
+
+# names_df.printSchema()
+# names_df.show()
+
+# # ...and join to the aggregates
+# joint_df = stats_df.join(names_df, stats_df.customer_id == names_df.id)
+# joint_df.printSchema()
+# joint_df.show()
 
