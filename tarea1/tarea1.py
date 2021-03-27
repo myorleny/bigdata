e2cbf756ff5978a65bba7fce2ff2f7be655e5674
Yorleni Alfaro <yorlenialfaro@gmail.com>
2021-03-27 15:12:39 -0600

    Se creó función obtener_topN_ciclistas_por_provincia_en_total_de_kilometros

--- tarea1/tarea1.py
+++ tarea1/tarea1.py
@@ -1,7 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import (IntegerType, FloatType, StructField,
                                StructType, TimestampType, StringType, DateType)
-from pyspark.sql.functions import col, date_format, udf                               
+from pyspark.sql.functions import col, date_format, udf, rank                              
+from pyspark.sql.window import Window
 
 
 spark = SparkSession.builder.appName("Read Transactions").getOrCreate()
@@ -16,7 +17,7 @@ def unir_dataframes(ciclista_df, ruta_df, actividad_df):
   
     return ciclista_actividad_ruta_df
 
-# Obtiene los kilómetros por ciclista
+# Obtiene kilómetros recorridos por ciclista, por ruta, por provincia y por día
 def obtener_kilometros_por_ciclista (ciclista_actividad_ruta_df):
     
     filter_df = ciclista_actividad_ruta_df.filter(ciclista_actividad_ruta_df.kilometros > 0)
@@ -35,6 +36,26 @@ def obtener_kilometros_por_ciclista (ciclista_actividad_ruta_df):
 
     return ciclistas_kilometros_df
 
+# Obtiene el top N de ciclistas por provincia, en total de kilómetros 
+def obtener_topN_ciclistas_por_provincia_en_total_de_kilometros (ciclistas_kilometros_df, N):
+    
+    provincia_ciclistas_kilometros_df = ciclistas_kilometros_df.groupBy("provincia", "cedula", "nombre_Completo").sum("TotalKilometros")
+    provincia_ciclistas_kilometros_df.show()
+
+    provincia_ciclistas_kilometros_df = \
+    provincia_ciclistas_kilometros_df.select(
+        col('provincia'),
+        col('cedula'),
+        col('nombre_Completo'),
+        col('sum(TotalKilometros)').alias('TotalKilometros'))
+    provincia_ciclistas_kilometros_df.show()
+
+    window = Window.partitionBy('provincia').orderBy(col('TotalKilometros').desc())
+    provincia_ciclistas_kilometros_df = provincia_ciclistas_kilometros_df.withColumn("Posicion_Por_Provincia",rank().over(window))
+	
+    provincia_ciclistas_kilometros_df = provincia_ciclistas_kilometros_df.filter(provincia_ciclistas_kilometros_df.Posicion_Por_Provincia <= N)
+	
+    return provincia_ciclistas_kilometros_df
 
 def programaPrincipal():
 
@@ -79,7 +100,11 @@ def programaPrincipal():
 
     ciclistas_kilometros_df.show()
 
+    N = 1
+    provincia_ciclistas_kilometros_df = obtener_topN_ciclistas_por_provincia_en_total_de_kilometros (ciclistas_kilometros_df, N)
+    provincia_ciclistas_kilometros_df.show()
 
+ 
 programaPrincipal()
 
 
